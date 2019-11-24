b49ee2fd2de161d2059510bc190eeea770b1e0d6
Eugene Kovaljov <ykovalov@homeaway.com>
2019-11-24 14:17:37 +0200

    Finish firebase file uploading

--- packages/raccoon-city-graphql/src/firebase/index.ts
+++ packages/raccoon-city-graphql/src/firebase/index.ts
@@ -5,7 +5,7 @@ const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 export function initFirebase() {
     return admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://raccoon-city-c14f4.firebaseio.com",
-        storageBucket: "gs://raccoon-city-c14f4.appspot.com"
+        databaseURL: "https://raccoon-city-74eff.firebaseio.com",
+        storageBucket: "gs://raccoon-city-74eff.appspot.com/"
     });
 }
--- packages/raccoon-city-graphql/src/resolvers/mutations/apartmentComplex.ts
+++ packages/raccoon-city-graphql/src/resolvers/mutations/apartmentComplex.ts
@@ -1,9 +1,12 @@
 import {Context} from '../../utils';
 import {ApartmentComplexInputArgs} from '../../types/apartment_complex';
 import ApartmentComplexModel, {ApartmentComplex} from '../../db/models/apartmentComplex';
-import * as firebase from 'firebase';
+import { uuid } from 'uuidv4';
 import * as fs from 'fs';
 
+function getFileExtension(fileName: string): string {
+    return fileName.split('.').pop();
+}
 export const apartmentComplex = {
     async createApartmentComplex(parent, args, ctx: Context): Promise<ApartmentComplex> {
         const apartmentComplex: ApartmentComplexInputArgs = args.apartmentComplex;
@@ -12,27 +15,35 @@ export const apartmentComplex = {
     async addImage(parent, {file: filePromise}, ctx: Context) {
         const {Firebase} = ctx;
         const file = await filePromise;
-
-        const writeStream = fs.createWriteStream(`./uploadedFiles/${file.filename}`);
-        const fileStream = file.createReadStream();
-        fileStream.pipe(writeStream);
-
-        /*const bucket = await Firebase.storage().bucket().upload(`${__dirname}/tmp/${fileName}`, {
-            gzip: true,
-            metadata: {
-                cacheControl: 'public, max-age=31536000',
-            }});*/
-        /*const uploadTask: firebase.storage.UploadTask = storageRef.put(file);
-        const promise = new Promise<string>((resolve, reject) => {
-            uploadTask.on('state_changed', () => {
-                uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                    resolve(downloadURL);
+        const newFileName = `${uuid()}.${getFileExtension(file.filename)}`;
+        const fileUrl = `./uploadedFiles/${newFileName}`;
+        try {
+            const writeStream = fs.createWriteStream(fileUrl);
+            const fileStream = file.createReadStream();
+            const pipedStream = fileStream.pipe(writeStream);
+            await new Promise((resolve, reject) => {
+                pipedStream.on('finish', () => {
+                    resolve();
                 });
             });
-        });
-        const downloadUrl = await promise;*/
-        return {
-            downloadUrl: ''
-        };
+            const bucket = Firebase.storage().bucket();
+
+            const uploadedFileData = await bucket.upload(fileUrl, {
+                gzip: true,
+                metadata: {
+                    cacheControl: 'public, max-age=31536000'
+                }
+            });
+            const fileData = uploadedFileData[0];
+            const config = {action: 'read', expires: '01-01-2025'};
+            // @ts-ignore
+            const [downloadUrl] = await fileData.getSignedUrl(config);
+            return {
+                downloadUrl
+            };
+        } finally {
+            fs.unlinkSync(fileUrl);
+        }
+
     }
 };
