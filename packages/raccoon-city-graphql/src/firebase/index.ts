f7fa37d10bf3908861471e3f05092909672e1f80
Eugene Kovaljov <ykovalov@homeaway.com>
2019-11-25 19:52:32 +0200

    test

--- packages/raccoon-city-graphql/src/firebase/index.ts
+++ packages/raccoon-city-graphql/src/firebase/index.ts
@@ -1,11 +1,21 @@
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
-
 export function initFirebase() {
     return admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://raccoon-city-74eff.firebaseio.com",
-        storageBucket: "gs://raccoon-city-74eff.appspot.com/"
+        credential: admin.credential.cert({
+                'type': 'service_account',
+                'project_id': 'raccoon-city-74eff',
+                'private_key_id': process.env.FB_PRIVATE_KEY_ID,
+                'private_key': process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                'client_email': 'firebase-adminsdk-ahgyi@raccoon-city-74eff.iam.gserviceaccount.com',
+                'client_id': '116806846704337226040',
+                'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+                'token_uri': 'https://oauth2.googleapis.com/token',
+                'auth_provider_x509_cert_url': 'https://www.googleapis.com/oauth2/v1/certs',
+                'client_x509_cert_url': 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ahgyi%40raccoon-city-74eff.iam.gserviceaccount.com'
+            } as any
+        ),
+        databaseURL: 'https://raccoon-city-74eff.firebaseio.com',
+        storageBucket: 'gs://raccoon-city-74eff.appspot.com/'
     });
 }
