9f9b09c5b0f4510be588c6ab4734ac5a80e24b0e
Eugene Kovaljov <ykovalov@homeaway.com>
2020-01-02 19:01:22 +0200

    Add reorder and removal for levels

--- packages/raccoon-city-ui/src/components/Main/HouseBuilder/HouseEditor/LevelRepresentation/LevelRepresentation.tsx
+++ packages/raccoon-city-ui/src/components/Main/HouseBuilder/HouseEditor/LevelRepresentation/LevelRepresentation.tsx
@@ -1,17 +1,22 @@
 import {useMutation} from '@apollo/react-hooks';
-import {Button, ExpansionPanelSummary} from '@material-ui/core';
+import {Button, ExpansionPanelSummary, FormControlLabel} from '@material-ui/core';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import React, {Fragment, memo} from 'react';
+import {useParams} from 'react-router-dom';
+import {SortableContainer, SortableElement, SortableHandle} from 'react-sortable-hoc';
 import styled from 'styled-components';
+import {Section} from '../../../../../../../raccoon-city-graphql/src/db/models/section';
 import {ADD_LEVEL} from '../../../../../graphql/mutations/flatMutation';
 import {GroupedFlats} from '../../../../../graphql/queries/houseQuery';
 import {AddFlatCard} from '../AddFlatCard/AddFlatCard';
 import {FlatCard} from '../FlatCard/FlatCard';
-import {useParams} from 'react-router-dom';
 
 interface LevelRepresentationProps {
     section: GroupedFlats;
@@ -21,9 +26,21 @@ const StyledButton = styled(Button)`
     margin-bottom: 16px !important;
 `;
 
+const StyledLabel = styled(FormControlLabel)`
+    margin-left: auto !important;
+    margin-right: 0 !important;
+`;
+
+const StyledInfo = styled(Typography)`
+    align-self: center;
+`;
+
 interface AddLevelButtonProps {
     section: string;
 }
+
+const DragHandle = SortableHandle(() => <DehazeIcon />);
+
 function AddLevelButton({section}: AddLevelButtonProps) {
     const [addLevel] = useMutation(ADD_LEVEL);
     const {houseUuid: uuid} = useParams();
@@ -45,25 +62,33 @@ function AddLevelButton({section}: AddLevelButtonProps) {
     );
 }
 
-export const LevelRepresentation = memo(function LevelRepresentationFn(props: LevelRepresentationProps) {
-    const {section} = props;
+const SortableItem = SortableElement(ExpansionPanel);
+const SortableList = SortableContainer(({section}: LevelRepresentationProps) => {
     return (
-        <Fragment>
-            <AddLevelButton section={section.section} />
-            {section.levels.map((level) => {
+        <div>
+            {section.levels.map((level, i) => {
                 return (
-                    <ExpansionPanel key={level.id} TransitionProps={{unmountOnExit: true}}>
+                    <SortableItem key={level.id} index={i} TransitionProps={{unmountOnExit: true}}>
                         <ExpansionPanelSummary
                             expandIcon={<ExpandMoreIcon />}
                             aria-controls="panel1a-content"
                             id="panel1a-header"
                         >
-                            <Typography>Этаж {level.level}</Typography>
+                            <FormControlLabel label="" control={<DragHandle />} />
+                            <StyledInfo>Этаж {level.level}</StyledInfo>
+                            <StyledLabel
+                                label=""
+                                control={
+                                    <IconButton color="secondary">
+                                        <DeleteOutline />
+                                    </IconButton>
+                                }
+                            />
                         </ExpansionPanelSummary>
                         <ExpansionPanelDetails>
                             <Grid container={true} spacing={3}>
                                 <Grid container={true} spacing={3} item={true} xs={3}>
-                                    <AddFlatCard level={level.level} section={props.section.section} />
+                                    <AddFlatCard level={level.level} section={section.section} />
                                 </Grid>
                                 {level.flats.map((flat) => {
                                     return (
@@ -74,9 +99,19 @@ export const LevelRepresentation = memo(function LevelRepresentationFn(props: Le
                                 })}
                             </Grid>
                         </ExpansionPanelDetails>
-                    </ExpansionPanel>
+                    </SortableItem>
                 );
             })}
+        </div>
+    );
+});
+
+export const LevelRepresentation = memo(function LevelRepresentationFn(props: LevelRepresentationProps) {
+    const {section} = props;
+    return (
+        <Fragment>
+            <AddLevelButton section={section.section} />
+            <SortableList useDragHandle={true} section={section} />
         </Fragment>
     );
 });
