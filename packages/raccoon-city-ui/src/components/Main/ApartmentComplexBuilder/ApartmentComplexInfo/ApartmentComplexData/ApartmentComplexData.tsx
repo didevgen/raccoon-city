eaa8412a7f31a5f8175206191319eb64cd4c1333
Eugene Kovaljov <ykovalov@homeaway.com>
2020-04-10 19:28:38 +0300

    test

--- packages/raccoon-city-ui/src/components/Main/ApartmentComplexBuilder/ApartmentComplexInfo/ApartmentComplexData/ApartmentComplexData.tsx
+++ packages/raccoon-city-ui/src/components/Main/ApartmentComplexBuilder/ApartmentComplexInfo/ApartmentComplexData/ApartmentComplexData.tsx
@@ -1,10 +1,10 @@
+import * as React from 'react';
+
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
-import * as React from 'react';
-import styled from 'styled-components';
 import {ApartmentComplexType} from '../../../../shared/types/apartmentComplex.types';
 
 interface ApartmentComplexDataProps {
