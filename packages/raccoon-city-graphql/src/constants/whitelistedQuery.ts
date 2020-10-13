82dd27d342ebb2fa74e27f3cf87f74ce19401a78
Eugene Kovaljov <didevgen@gmail.com>
2020-10-13 16:34:41 +0300

    make public queries whitelisted

--- packages/raccoon-city-graphql/src/constants/whitelistedQuery.ts
+++ packages/raccoon-city-graphql/src/constants/whitelistedQuery.ts
@@ -1,4 +1,12 @@
-export const WHITELISTED_QUERIES = ['login', 'createUser', 'authorizeAmo'];
+export const WHITELISTED_QUERIES = ['login', 'createUser', 'authorizeAmo',
+
+
+
+    'getPublicApartmentComplexesByDeveloper',
+    'getPublicGroupedFlatsBySection',
+    'getPublicFlatSidebarInfo',
+    'getPublicFlatsList',
+    'getApartmentComplexLayout'];
 
 export const PUBLIC_QUERIES = [
     'getPublicApartmentComplexesByDeveloper',
