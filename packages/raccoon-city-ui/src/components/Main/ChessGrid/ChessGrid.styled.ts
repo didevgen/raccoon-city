dbde4879cc71f917e7940fce0efe9820e9cff7a1
letrylesha <lunarboxnote@gmail.com>
2020-11-07 17:15:07 +0300

    add house bar

--- packages/raccoon-city-ui/src/components/Main/ChessGrid/ChessGrid.styled.ts
+++ packages/raccoon-city-ui/src/components/Main/ChessGrid/ChessGrid.styled.ts
@@ -5,6 +5,15 @@ import Select from '@material-ui/core/Select';
 export const ChessGridWrapper: any = styled.div`
     display: flex;
     flex-direction: row;
+    justify-content: center;
+    min-width: 700px;
+    margin-top: 20px;
+
+    @media only screen and (max-width: 600px) {
+        min-width: 320px;
+        overflow-x: auto;
+        background-color: #fff;
+    }
 `;
 
 export const ColumnWrapper = styled.div`
@@ -19,6 +28,16 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-self: flex-end;
+    width: 100%;
+`;
+
+export const ColumnAndSectionBarWrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+
+    @media only screen and (max-width: 600px) {
+        flex-direction: column;
+    }
 `;
 
 export const ColumnTitle = styled(Typography)`
@@ -35,4 +54,29 @@ export const SidebarDrawer = styled(SwipeableDrawer)`
 
 export const SelectStyled = styled(Select)`
     margin: 0 20px;
+`;
+
+export const MobileInformation = styled.div`
+    display: none;
+
+    @media only screen and (max-width: 600px) {
+        display: flex;
+        justify-content: flex-start;
+    }
+`;
+
+export const InfoIcon = styled.div`
+    border: 1px solid #e84f1d;
+    border-radius: 8px;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &::before {
+        content: 'i';
+        font-size: 15px;
+        color: #e84f1d;
+    }
 `;
--- packages/raccoon-city-ui/src/components/Main/ChessGrid/SectionBar/SectionBar.tsx
+++ packages/raccoon-city-ui/src/components/Main/ChessGrid/SectionBar/SectionBar.tsx
@@ -1,1 +1,126 @@
-import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import {AppBar} from '@material-ui/core';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import CloseIcon from '@material-ui/icons/Close';
+import React, {useState} from 'react';
+import {useParams} from 'react-router';
+import {APARTMENT_COMPLEX_INFO} from '../../../../graphql/queries/apartmentComplexQuery';
+import {ApartmentComplexType, ImageType} from '../../../shared/types/apartmentComplex.types';
+import {ApartmentComplexData} from '../../ApartmentComplexBuilder/ApartmentComplexInfo/ApartmentComplexData/ApartmentComplexData';
+import {TabPanel} from '../../ApartmentComplexBuilder/ApartmentComplexInfo/ApartmentComplexInfo';
+import {SidebarVRDialog} from '../FlatSidebarInfo/SidebarVRDialog';
+import {
+    AppBarContainer,
+    CloseBarContainer,
+    CustomSlider,
+    SectionBarContainer,
+    SideBarImage,
+    SideBarVRImage,
+    SliderContainer,
+    TabPanelContainer
+} from './SectionBar.styled';
+
+// TODO add error handle
+export const SectionBar = (props: any) => {
+    const {isSideBarOpen, setSideBarOpen} = props;
+
+    const {apartmentComplexUuid} = useParams() as any;
+    const [value, setValue] = useState(0);
+    const [open, setOpen] = useState(false);
+    const [downloadLink, setDownloadLink] = React.useState('');
+    const [name, setName] = React.useState('');
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const {loading, error, data} = useQuery<{getApartmentComplex: ApartmentComplexType}>(APARTMENT_COMPLEX_INFO, {
+        fetchPolicy: 'cache-and-network',
+        variables: {
+            uuid: apartmentComplexUuid
+        }
+    });
+
+    if (loading) {
+        return <div>Loading</div>;
+    }
+
+    if (error) {
+        return <div>Error :(</div>;
+    }
+
+    if (!data || !apartmentComplexUuid) {
+        return <div>Error</div>;
+    }
+
+    const {images} = data.getApartmentComplex;
+
+    const photosJSX = images?.PHOTO?.map(({downloadUrl, uuid}) => (
+        <SideBarImage key={uuid} src={downloadUrl} alt="house image" />
+    ));
+
+    return (
+        <SectionBarContainer isSideBarOpen={isSideBarOpen}>
+            <CloseBarContainer>
+                <CloseIcon onClick={() => setSideBarOpen(false)} />
+            </CloseBarContainer>
+
+            <SliderContainer>
+                <CustomSlider itemsToShow={1} pagination={false} disableArrowsOnEnd={false}>
+                    {photosJSX}
+                </CustomSlider>
+            </SliderContainer>
+
+            <AppBarContainer>
+                <AppBar position="static" color="default">
+                    <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                        <Tab label="Информация" />
+                        <Tab label="360" />
+                        <Tab label="Фото" />
+                    </Tabs>
+                </AppBar>
+            </AppBarContainer>
+            <TabPanelContainer value={value} index={0}>
+                <ApartmentComplexData apartmentComplex={data.getApartmentComplex} />
+            </TabPanelContainer>
+            <TabPanel value={value} index={1}>
+                {!images?.VR ? (
+                    <div>Нет VR фото</div>
+                ) : (
+                    images.VR.map(({downloadUrl, uuid, name}) => (
+                        <SideBarVRImage
+                            key={uuid}
+                            src={downloadUrl}
+                            alt="house image"
+                            onClick={() => {
+                                setDownloadLink(downloadUrl);
+                                setName(name);
+                                setOpen(true);
+                            }}
+                        />
+                    ))
+                )}
+            </TabPanel>
+            <TabPanelContainer value={value} index={2}>
+                {!images?.PHOTO ? (
+                    <div>Нет фото</div>
+                ) : (
+                    <div style={{display: 'flex', flexDirection: 'column', maxHeight: '600px', overflowY: 'auto'}}>
+                        {photosJSX}
+                    </div>
+                )}
+            </TabPanelContainer>
+
+            <SidebarVRDialog
+                setOpen={setOpen}
+                open={open}
+                mode={ImageType.VR}
+                downloadLink={downloadLink}
+                name={name}
+            />
+        </SectionBarContainer>
+    );
+};
