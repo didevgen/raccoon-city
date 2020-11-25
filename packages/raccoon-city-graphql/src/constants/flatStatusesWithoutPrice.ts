f93d939408f5866ad12d2b7a5b7d6791255d72e0
SalischevAV <devorer77@gmail.com>
2020-11-25 17:39:27 +0200

    hot fix

--- packages/raccoon-city-graphql/src/constants/flatStatusesWithoutPrice.ts
+++ packages/raccoon-city-graphql/src/constants/flatStatusesWithoutPrice.ts
@@ -1,7 +1,1 @@
-export const flatStatusesWithoutPrice = [
-  'SOLD_OUT',
-  'RESERVED',
-  'DOCUMENTS_IN_PROGRESS',
-  'UNAVAILABLE',
-  'BOOKED',
-];
+export const flatStatusesWithoutPrice = ['SOLD_OUT', 'RESERVED', 'DOCUMENTS_IN_PROGRESS', 'UNAVAILABLE', 'BOOKED'];
